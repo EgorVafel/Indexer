6242e24fbd5023e35a5bff05727bf2655aa2ad72
zaxar163 <zahar.vcherachny@yandex.ru>
2019-01-09 21:14:43 +0400

    Moved to gradle + reformat source code.

--- src/main/java/net/xtrafrancyz/degustator/command/CommandManager.java
+++ src/main/java/net/xtrafrancyz/degustator/command/CommandManager.java
@@ -1,50 +1,46 @@
 package net.xtrafrancyz.degustator.command;
 
-import sx.blah.discord.handle.obj.IMessage;
-
-import net.xtrafrancyz.degustator.Degustator;
-
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * @author xtrafrancyz
- */
+import net.xtrafrancyz.degustator.Degustator;
+import sx.blah.discord.handle.obj.IMessage;
+
 public class CommandManager {
-    public final Degustator app;
-    public Map<String, Command> registered;
-    public Map<String, Command> alises;
-    
-    public CommandManager(Degustator app) {
-        this.app = app;
-        registered = new HashMap<>();
-        alises = new HashMap<>();
-    }
-    
-    public void registerCommand(Command command) {
-        this.registered.put(command.command, command);
-        for (String cmd : command.aliases)
-            alises.put(cmd, command);
-    }
-    
-    public Command getCommand(String cmd) {
-        Command command = registered.get(cmd);
-        if (command != null)
-            return command;
-        return alises.get(cmd);
-    }
-    
-    public void process(IMessage message) {
-        try {
-            String text = message.getContent().substring(1);
-            String[] args = text.split(" ");
-            Command command = getCommand(args[0].toLowerCase());
-            if (command == null || !command.canUse(message))
-                return;
-            command.onCommand(message, Arrays.copyOfRange(args, 1, args.length));
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+	public final Degustator app;
+	public Map<String, Command> registered;
+	public Map<String, Command> alises;
+
+	public CommandManager(Degustator app) {
+		this.app = app;
+		registered = new HashMap<>();
+		alises = new HashMap<>();
+	}
+
+	public Command getCommand(String cmd) {
+		Command command = registered.get(cmd);
+		if (command != null)
+			return command;
+		return alises.get(cmd);
+	}
+
+	public void process(IMessage message) {
+		try {
+			String text = message.getContent().substring(1);
+			String[] args = text.split(" ");
+			Command command = getCommand(args[0].toLowerCase());
+			if (command == null || !command.canUse(message))
+				return;
+			command.onCommand(message, Arrays.copyOfRange(args, 1, args.length));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void registerCommand(Command command) {
+		registered.put(command.command, command);
+		for (String cmd : command.aliases)
+			alises.put(cmd, command);
+	}
 }
--- src/main/java/net/xtrafrancyz/degustator/mysql/PreparedStatementFiller.java
+++ src/main/java/net/xtrafrancyz/degustator/mysql/PreparedStatementFiller.java
@@ -3,9 +3,6 @@ package net.xtrafrancyz.degustator.mysql;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-/**
- * @author xtrafrancyz
- */
 public interface PreparedStatementFiller {
-    void fill(PreparedStatement statement) throws SQLException;
+	void fill(PreparedStatement statement) throws SQLException;
 }
