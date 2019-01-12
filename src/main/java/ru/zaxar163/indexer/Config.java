5b16956207e7fb0928b7a51901251759c8882498
zaxar163 <zahar.vcherachny@yandex.ru>
2019-01-12 17:04:37 +0400

    Config entry `url` removed.

--- src/main/java/ru/zaxar163/indexer/Config.java
+++ src/main/java/ru/zaxar163/indexer/Config.java
@@ -1,21 +1,16 @@
 package ru.zaxar163.indexer;
 
-/**
- * @author xtrafrancyz
- */
 public class Config {
 	public static class Mysql {
-		public String url = "jdbc:mysql://127.0.0.1/degustator?useUnicode=true&characterEncoding=utf-8";
-		public String user = "root";
-		public String pass = "";
+		public String user = "db_user";
+		public String pass = "db_pass";
 		public int port = 336;
-		public String host = "";
+		public String host = "db.host";
 		public boolean useSSL = false;
 		public boolean verifyCertificates = false;
-		public String database = "bot";
+		public String database = "indexer";
 	}
 
 	public String token = "MY_BOT_TOKEN";
-
 	public Mysql mysql = new Mysql();
 }
